45ce604bfc54c92d88999a634f1bfccc2ec0ee99
haxvendas <thiagohax9@gmail.com>
2025-05-09 19:54:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function abrirCarta() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.style.display = 'flex';
+    mensagem.style.flexDirection = 'column';
+    mensagem.style.justifyContent = 'center';
+    mensagem.style.alignItems = 'center';
+}
